08fd5cdadda4f0df0af7c3553b96b2b74125e93e
SmileyDev <nemanja.gakovic@hotmail.com>
2021-06-11 12:29:56 +0200

    Debounce search news

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,36 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useMemo } from "react";
 import classes from "./Search.module.css";
 import Axios from "../../Axios";
-import { debounce } from "lodash";
+import debounce from "lodash.debounce";
+import ArticleThumbnail from "../ArticleThumbnail/ArticleThumbnail";
 
 function Search() {
   const [news, setNews] = useState({ articles: [] });
   const [search, setSearch] = useState("");
 
-  const fetchNews = debounce(async (search) => {
-    try {
-      const result = await Axios.get("/", {
-        params: {
-          country: "us",
-          q: search,
-        },
-      });
-      console.log(result.data);
-      setNews(result.data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, 500);
+  const debouncedFetchNews = useMemo(
+    () =>
+      debounce(async (search) => {
+        try {
+          const result = await Axios.get("/", {
+            params: {
+              country: "us",
+              q: search,
+            },
+          });
+          setNews(result.data);
+        } catch (error) {
+          console.log(error.message);
+        }
+      }, 500),
+    [],
+  );
 
   const handleSearch = (e) => {
     setSearch(e.target.value);
-    fetchNews(search);
-    console.log(news);
+    debouncedFetchNews(search);
   };
-
-  /*   useEffect(() => {
-    fetchNews(search);
-  }, [search, fetchNews]); */
 
   return (
     <div className={classes.Search}>
@@ -39,11 +38,18 @@ function Search() {
         type="text"
         onChange={handleSearch}
       />
-      <div>
+      <div className={classes.searchResults}>
         {news.articles.length === 0 ? (
           <h1>Search</h1>
         ) : (
-          news.articles.map((article) => <h1>{article.title}</h1>)
+          news.articles.map((article) => {
+            if (article.content) {
+              return (
+                <ArticleThumbnail key={article.publishedAt} article={article} />
+              );
+            }
+            return null;
+          })
         )}
       </div>
     </div>
